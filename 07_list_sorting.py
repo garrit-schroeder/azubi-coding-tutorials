306736ab95716ef5234e16acacb666fe669dc024
Garrit Schroeder <wl_g_github@garrit-schroeder.de>
2021-11-23 11:39:58 +0100

    add sorting list

---
+++ 07_list_sorting.py
@@ -0,0 +1,9 @@
+# sort the list "my_list" with the lowest number first and the highest number last.
+
+def sort():
+    pass
+
+
+if __name__ == '__main__':
+    my_list = [1, 80, 35, 12, 57, 3, 11, 604, 13, 35]
+    sort()
